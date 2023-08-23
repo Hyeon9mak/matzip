58b7c65a05d7c7e1de4263a9968582e4ac4f81bc
Hyeon9mak <jinha3507@gmail.com>
2023-08-23 10:03:21 +0900

    initial commit

---
+++ src/main/kotlin/io/github/hyeon9mak/matzip/MatzipApplication.kt
@@ -0,0 +1,11 @@
+package io.github.hyeon9mak.matzip
+
+import org.springframework.boot.autoconfigure.SpringBootApplication
+import org.springframework.boot.runApplication
+
+@SpringBootApplication
+class MatzipApplication
+
+fun main(args: Array<String>) {
+    runApplication<MatzipApplication>(*args)
+}
